9cc8fe9da01827fb058f26973ca852755118bc67
Bellets <bellets96@gmail.com>
2024-04-25 17:50:29 +0200

    Modificata logica per incorporare anche il ruolo dell'utente nella richiesta

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,6 @@
-/* const { Events } = require("discord.js");
+const { Events } = require("discord.js");
 const fetch = require("node-fetch");
-const { apiUri } = require("../config.json");
+const { apiUri, allowedRoles } = require("../config.json");
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -26,6 +26,21 @@ module.exports = {
         interaction.customId === "entrata" ||
         interaction.customId === "uscita"
       ) {
+        const userRoles = interaction.member.roles.cache.map((role) => role.id);
+
+        const userAllowedRoles = userRoles.filter((roleId) =>
+          allowedRoles.includes(roleId)
+        );
+
+        if (userAllowedRoles.length === 0) {
+          console.error(`User does not have a role included in allowedRoles.`);
+          return;
+        }
+
+        const userRole = interaction.member.roles.cache.find(
+          (role) => role.id === userAllowedRoles[0]
+        );
+
         try {
           const timbraturaResponse = await fetch(apiUri + "/timbratrice", {
             method: "POST",
@@ -35,83 +50,7 @@ module.exports = {
             body: JSON.stringify({
               type: interaction.customId,
               userId: interaction.user.id,
-            }),
-          });
-
-          const res = await timbraturaResponse.json();
-          if (timbraturaResponse.ok) {
-            await interaction.reply({
-              content: res,
-              ephemeral: true,
-            });
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      } else {
-        return;
-      }
-    }
-  },
-};
- */
-
-const { Events } = require("discord.js");
-const fetch = require("node-fetch");
-const { apiEndpoints } = require("../config.json");
-
-module.exports = {
-  name: Events.InteractionCreate,
-  async execute(interaction) {
-    if (interaction.isChatInputCommand()) {
-      const command = interaction.client.commands.get(interaction.commandName);
-
-      if (!command) {
-        console.error(
-          `No command matching ${interaction.commandName} was found.`
-        );
-        return;
-      }
-
-      try {
-        await command.execute(interaction);
-      } catch (error) {
-        console.error(`Error executing ${interaction.commandName}`);
-        console.error(error);
-      }
-    } else if (interaction.isButton()) {
-      if (
-        interaction.customId === "entrata" ||
-        interaction.customId === "uscita"
-      ) {
-        const userPermissions = interaction.member.roles.cache.map(
-          (role) => role.id
-        );
-        let endpoint;
-
-        for (const permission of userPermissions) {
-          if (apiEndpoints[permission]) {
-            endpoint = apiEndpoints[permission];
-            break;
-          }
-        }
-
-        if (!endpoint) {
-          console.error(
-            `No endpoint found for permissions: ${userPermissions}`
-          );
-          return;
-        }
-
-        try {
-          const timbraturaResponse = await fetch(endpoint, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              type: interaction.customId,
-              userId: interaction.user.id,
+              role: userRole.name,
             }),
           });
 
@@ -125,8 +64,6 @@ module.exports = {
         } catch (error) {
           console.error(error);
         }
-      } else {
-        return;
       }
     }
   },
